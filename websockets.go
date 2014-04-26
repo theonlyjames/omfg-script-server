7ab91e035b0f4d885b52f2f5c6f18a30ec8c67d8
Ubuntu <ubuntu@ip-172-31-28-35.ec2.internal>
2014-04-26 01:58:35 +0000

    OH MY FUCKING GOD ITS ALIVEgit add -A ! ITS ALIVEgit add -A !

--- websockets.go
+++ websockets.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"code.google.com/p/google-api-go-client/mirror/v1"
+	//"code.google.com/p/google-api-go-client/mirror/v1"
 	"github.com/wearscript/wearscript-go/wearscript"
 	"fmt"
-	"strings"
+	//"strings"
 	"time"
 	"sync"
 )
@@ -23,40 +23,51 @@ func WSHandler(ws *websocket.Conn) {
 	fmt.Println("Connected with glass")
 	fmt.Println(ws.Request().URL.Path)
 	userId, err := userID(ws.Request())
-        if err != nil || userId == "" {
-                path := strings.Split(ws.Request().URL.Path, "/")
-		fmt.Println(path)
-                if len(path) != 3 {
-                        fmt.Println("Bad path")
-                        return
-                }
-                userId, err = getSecretUser("ws", secretHash(path[len(path)-1]))
-                if err != nil {
-                        fmt.Println(err)
-                        return
-                }
-        }
-	userId = SanitizeUserId(userId)
-	_, err = mirror.New(authTransport(userId).Client()) // svc
-	if err != nil {
-		LogPrintf("ws: mirror")
-		//WSSend(c, &[]interface{}{[]uint8("error"), []uint8("Unable to create mirror transport")})
-		return
-	}
+    //    if err != nil || userId == "" {
+    //            path := strings.Split(ws.Request().URL.Path, "/")
+	//	fmt.Println(path)
+    //            if len(path) != 3 {
+    //                    fmt.Println("Bad path")
+    //                    return
+    //            }
+    //            userId, err = getSecretUser("ws", secretHash(path[len(path)-1]))
+    //            if err != nil {
+    //                    fmt.Println(err)
+    //                    return
+    //            }
+    //    }
+	//userId = SanitizeUserId(userId)
+	//, err = mirror.New(authTransport(userId).Client()) // svc
+	//if err != nil {
+	//	LogPrintf("ws: mirror")
+	//	//WSSend(c, &[]interface{}{[]uint8("error"), []uint8("Unable to create mirror transport")})
+	//	return
+	//}
 	// TODO(brandyn): Add lock here when adding users
 	if Managers[userId] == nil {
-		Managers[userId], err = wearscript.ConnectionManagerFactory("server", "0")
+		Managers[userId], err = wearscript.ConnectionManagerFactory("server", "demo")
 		if err != nil {
 			LogPrintf("ws: cm")
 			return
 		}
-		Managers[userId].SubscribeTestHandler()
-		Managers[userId].Subscribe("gist", func(channel string, dataRaw []byte, data []interface{}) {
-			GithubGistHandle(Managers[userId], userId, data)
-		})
-		Managers[userId].Subscribe("weariverse", func(channel string, dataRaw []byte, data []interface{}) {
-			WeariverseGistHandle(Managers[userId], userId, data)
-		})
+        Managers[userId].Subscribe("image", func (c string, dataBin []byte, data []interface{}) {
+            timestamp := data[1].(float64)
+            image := data[2].(string)
+            fmt.Println(fmt.Sprintf("Image[%s] Time[%f] Bytes[%d]", c, timestamp,           len(image)))
+        })
+  
+        Managers[userId].Subscribe("sensors", func (c string, dataBin []byte, data []interface{}) {
+            names := data[1]
+            samples := data[2]
+            fmt.Println(fmt.Sprintf("Sensors[%s]\n Names[%v]\n Samples[%v]\n", c, names,    samples))
+        })
+		//Managers[userId].SubscribeTestHandler()
+		//Managers[userId].Subscribe("gist", func(channel string, dataRaw []byte, data []interface{}) {
+		//	GithubGistHandle(Managers[userId], userId, data)
+		//})
+		//Managers[userId].Subscribe("weariverse", func(channel string, dataRaw []byte, data []interface{}) {
+		//	WeariverseGistHandle(Managers[userId], userId, data)
+		//})
 	}
 	cm := Managers[userId]
 	conn, err := cm.NewConnection(ws) // con
